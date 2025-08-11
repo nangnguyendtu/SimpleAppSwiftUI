7f78032031272b1d36ba4318e864d0716835730c
nnnguyen-ios <nnnguyen@enjoyworks.co.kr>
2025-08-11 14:24:21 +0700

    Build success

--- SimpleAppSwiftUI/SimpleAppSwiftUIApp.swift
+++ SimpleAppSwiftUI/SimpleAppSwiftUIApp.swift
@@ -2,9 +2,9 @@ import SwiftUI
 
 @main
 struct SimpleAppSwiftUIApp: App {
-//    var body: some Scene {
-//        WindowGroup {
-//            ContentView()
-//        }
-//    }
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
 }
