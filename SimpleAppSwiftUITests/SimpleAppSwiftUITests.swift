e51b985d7e59b012ccf7d0a57c109b0a63894609
nnnguyen-ios <nnnguyen@enjoyworks.co.kr>
2025-08-11 15:58:55 +0700

    Added unit test

---
+++ SimpleAppSwiftUITests/SimpleAppSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAppSwiftUITests.swift
+//  SimpleAppSwiftUITests
+//
+//  Created by Enjoyworks macmini on 11/8/25.
+//
+
+import Testing
+import Foundation
+
+struct SimpleAppSwiftUITests {
+
+    @Test func example() async throws {
+        throw NSError(domain: "TestError", code: 1, userInfo: nil)
+    }
+
+}
